f6ba91b1c11cf1b35c01adbf62331b0a115b9a3b
MarceloJoia <sitejoia@hotmail.com>
2022-01-10 17:10:33 -0300

    PUT - atualização deve atualizar o recurso quando o ID existirA

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -3,24 +3,45 @@ package com.devsuperior.bds02.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.exceptions.ResourceNotFoundException;
 import com.devsuperior.bds02.repositories.EventRepository;
 
 @Service
 public class EventService {
-	
+
 	@Autowired
 	private EventRepository repository;
 
 	@Transactional(readOnly = true)
 	public List<EventDTO> findAll() {
 		List<Event> list = repository.findAll();
-		
+
 		return list.stream().map(x -> new EventDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Event entity = repository.getOne(id);
+			CopyDtoToEvent(dto, entity);
+			entity = repository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	private void CopyDtoToEvent(EventDTO dto, Event entity) {
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setUrl(dto.getUrl());
 	}
 }
